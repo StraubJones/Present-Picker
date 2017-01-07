c8a6a93982cc81502c3aecd4bb364b32e1dc0096
Christian Straubhaar-Jones <christian.straubhaar@gmail.com>
2017-01-07 12:15:24 -0600

    adding sub-menus for selecting individual adj or noun values to include or not

--- app/scripts/controllers/picker.js
+++ app/scripts/controllers/picker.js
@@ -9,49 +9,186 @@
  */
 angular.module('presentPickerApp')
   .controller('PickerCtrl', ['$scope', function ($scope) {
-    $scope.greeting = 'Hi!';
+
+    $scope.greeting = 'Hi';
+
+    $scope.adjToAdd;
+
     $scope.allAdjectiveTypes = [
       {
         type: 'Color',
-        list: ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'black', 'grey'],
+        list: [
+          {
+            value: 'red',
+            active: true
+          },
+          {
+            value: 'orange',
+            active: true
+          },
+          {
+            value: 'yellow',
+            active: true
+          },
+          {
+            value: 'green',
+            active: true
+          },
+          {
+            value: 'blue',
+            active: true
+          },
+          {
+            value: 'purple',
+            active: true
+          },
+          {
+            value: 'black',
+            active: true
+          },
+          {
+            value: 'grey',
+            active: true
+          }
+        ],
         chosen: false
       },
       {
         type: 'Pattern',
-        list: ['polka-dotted', 'checkered', 'argyle', 'plaid'],
+        list: [
+          {
+            value: 'polka-dotted',
+            active: true
+          },
+          {
+            value: 'checkered',
+            active: true
+          },
+          {
+            value: 'argyle',
+            active: true
+          },
+          {
+            value: 'plaid',
+            active: true
+          }
+        ],
         chosen: false
       },
       {
         type: 'Size',
-        list: ['5-foot', 'one-foot', '6-inch'],
+        list: [
+          {
+            value: '5-foot',
+            active: true
+          },
+          {
+            value: 'one-foot',
+            active: true
+          },
+          {
+            value: '6-inch',
+            active: true
+          },
+        ],
         chosen: false
       },
     ];
     $scope.activeAdjectiveTypes = [];
+
     $scope.allNounTypes = [
       {
         type: 'Car',
-        list: ['Honda', 'Toyota', 'Subaru', 'Kia'],
+        list: [
+          {
+            value: 'Honda',
+            active: true
+          },
+          {
+            value: 'Toyota',
+            active: true
+          },
+          {
+            value: 'Subaru',
+            active: true
+          },
+          {
+            value: 'Kia',
+            active: true
+          }
+        ],
         chosen: false
       },
       {
         type: 'Lamp',
-        list: ['table lamp', 'floor lamp'],
+        list: [
+          {
+            value: 'table lamp',
+            active: true
+          },
+          {
+            value: 'floor lamp',
+            active: true
+          }
+        ],
         chosen: false
       },
       {
         type: 'Dress',
-        list: ['evening gown', 'sun dress', 'summer dress', 'wedding dress'],
+        list: [
+          {
+            value: 'evening gown',
+            active: true
+          },
+          {
+            value: 'sun dress',
+            active: true
+          },
+          {
+            value: 'summer dress',
+            active: true
+          },
+          {
+            value: 'wedding dress',
+            active: true
+          }
+        ],
         chosen: false
       },
       {
         type: 'Shirt',
-        list: ['t-shirt', 'button-down', 'polo'],
+        list: [
+          {
+            value: 't-shirt',
+            active: true
+          },
+          {
+            value: 'button-down',
+            active: true
+          },
+          {
+            value: 'polo',
+            active: true
+          }
+        ],
         chosen: false
       },
       {
         type: 'Pants',
-        list: ['slacks', 'jeans', 'corduroy pants'],
+        list: [
+          {
+            value: 'slacks',
+            active: true
+          },
+          {
+            value: 'jeans',
+            active: true
+          },
+          {
+            value: 'corduroy pants',
+            active: true
+          },
+        ],
         chosen: false
       },
     ];
@@ -60,13 +197,13 @@ angular.module('presentPickerApp')
 
     $scope.toggleAdj = (adj)=>{
       if(!$scope.activeAdjectiveTypes.includes(adj)) {
-        adj.current = adj.list[Math.floor(Math.random() * adj.list.length)]
+        adj.current = adj.list[Math.floor(Math.random() * adj.list.length)].value;
         $scope.activeAdjectiveTypes.push(adj);
       } else {
         $scope.activeAdjectiveTypes.splice($scope.activeAdjectiveTypes.indexOf(adj), 1);
       }
       adj.chosen = !adj.chosen;
-    }
+    };
 
     $scope.chooseNoun = (currNoun)=>{
       console.log('before:', $scope.activeNounObj, $scope.activeNoun);
@@ -75,7 +212,14 @@ angular.module('presentPickerApp')
       })
       currNoun.chosen = true;
       $scope.activeNounObj = currNoun;
-      $scope.activeNoun = currNoun.list[Math.floor(Math.random() * currNoun.list.length)];
+      $scope.activeNoun = currNoun.list[Math.floor(Math.random() * currNoun.list.length)].value;
       console.log('after:', $scope.activeNounObj, $scope.activeNoun);
-    }
+    };
+
+    $scope.addNoun = (noun)=>{
+      activeNounObj.list.push({
+        value: noun,
+        active: true
+      });
+    };
   }]);
